38e0478187d757e756c1662b7b1d86e8775b5c81
Abraham Owodunni <Owodunniabraham@gmail.com>
2022-10-15 02:47:08 +0200

    Update actions.py

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -5,15 +5,14 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
-from .secrets import secrets
 
 ################## 
 # BANCO DE DADOS #
 ################## 
 from pymongo import MongoClient
 
-client = MongoClient(secrets["CLUSTER"])
-db = client[secrets["DB_NAME"]]
+client = '' #MongoClient(secrets["CLUSTER"])
+db = '' #client[secrets["DB_NAME"]]
 
 #############
 # FUNCTIONS #
